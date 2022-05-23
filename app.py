6f0fba858e2b63f4e22c70fe7f231f334bb97ffb
ME3ZA <101148207+ME3ZA@users.noreply.github.com>
2022-05-23 23:03:13 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import *
+import json
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+from flask_cors import CORS
+
+
+filename = 'Final_xgboost_model.joblib'
+model = keras.models.load_model('filename')
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods = ['GET'])
+def home_page():
+    data_set = {'Page': 'Home', 'Message': "Let's get started and send me your image", 'Timestamp': time.time()}
+    json_dump = json.dumps(data_set)
+    return json_dump
+  
+if __name__=='__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
