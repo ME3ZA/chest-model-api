f75e0e028ee5cae656e43529c958675a913273b4
ME3ZA <101148207+ME3ZA@users.noreply.github.com>
2022-05-25 01:43:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,22 +4,49 @@ import os
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-
 from flask_cors import CORS
+
+from tensorflow.keras.preprocessing import image
+from flask import Flask, render_template, request
+#from werkzeug import secure_filename
+app = Flask(__name__)
+
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      #f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
+		
+#if __name__ == '__main__':
+ #  app.run(debug = True)
 
 
 filename = 'chest_model_balanced.h5'
 model = keras.models.load_model(filename)
 
+y=model.predict(f)
+ans=np.argmax(y,axis=1)
+print(ans)
+if (ans==0):
+    print("covid")
+elif (ans==1):
+    print("healthy")
+elif (ans==2):
+    print("Lung Tumor")
+else :
+    print("Pneumonia")
 
-app = Flask(__name__)
-CORS(app)
 
-@app.route('/', methods = ['GET'])
-def home_page():
-    data_set = {'Page': 'Home', 'Message': "Let's get started and send me your image"}
-    json_dump = json.dumps(data_set)
-    return json_dump
+#@app.route('/', methods = ['GET'])
+#def home_page():
+ #   data_set = {'Page': 'Home', 'Message': "Let's get started and send me your image"}
+  #  json_dump = json.dumps(data_set)
+   # return json_dump
   
 if __name__=='__main__':
     port = int(os.environ.get('PORT', 5000))
