e8ed0d903fbb05c870b6c3729c7e5c260dbbb74c
ME3ZA <101148207+ME3ZA@users.noreply.github.com>
2022-05-25 03:23:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,13 +22,12 @@ def upload_file():
     filename = 'chest_model_balanced.h5'
 	model = keras.models.load_model(filename)   
 	if request.method == 'POST':
-        f = request.files['file']
-      	f.save(secure_filename(f.filename))
- 		
-		test_image = image.load_img(f, target_size = (64, 64))
-        test_image = image.img_to_array(test_image)
-        test_image = np.expand_dims(test_image, axis = 0)
-        result = model.predict(test_image)
+    	f = request.files['file']
+    	f.save(secure_filename(f.filename))
+ 		test_image = image.load_img(f, target_size = (64, 64))
+    	test_image = image.img_to_array(test_image)
+    	test_image = np.expand_dims(test_image, axis = 0)
+    	result = model.predict(test_image)
 		ans=np.argmax(result,axis=1)
 		print(ans)
 		if (ans==0):
