4ec8deb8a7cc5d00d07abbbdea9c22380800df76
ME3ZA <101148207+ME3ZA@users.noreply.github.com>
2022-05-25 03:34:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,25 +19,25 @@ def upload_html():
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
-	filename = 'chest_model_balanced.h5'
-	model = keras.models.load_model(filename)   
-	if request.method == 'POST':
-    	f = request.files['file']
-    	f.save(secure_filename(f.filename))
-    	test_image = image.load_img(f, target_size = (64, 64))
-    	test_image = image.img_to_array(test_image)
-    	test_image = np.expand_dims(test_image, axis = 0)
-		result = model.predict(test_image)
-		ans=np.argmax(result,axis=1)
-		print(ans)
-		if (ans==0):
-			print("covid")
-		elif (ans==1):
-			print("healthy")
-		elif (ans==2):
-			print("Lung Tumor")
-		else :
-			print("Pneumonia")
+    filename = 'chest_model_balanced.h5'
+    model = keras.models.load_model(filename)   
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        test_image = image.load_img(f, target_size = (64, 64))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = model.predict(test_image)
+        ans=np.argmax(result,axis=1)
+        print(ans)
+        if (ans==0):
+            print("covid")
+        elif (ans==1):
+            print("healthy")
+        elif (ans==2):
+            print("Lung Tumor")
+        else :
+            print("Pneumonia")
 
 
 
