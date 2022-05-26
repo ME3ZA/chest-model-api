0ed97af1c7061e20b499aa7dd460de2fec039272
ME3ZA <101148207+ME3ZA@users.noreply.github.com>
2022-05-26 21:19:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,23 +4,47 @@ import os
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-
 from flask_cors import CORS
+from keras.models import load_model
+from keras.preprocessing import image
 
 
 filename = 'chest_model_balanced.h5'
 model = keras.models.load_model(filename)
 
+dic = {0 : 'Covid', 1 : 'Healthy' , 2 : 'Lung Tumor' , 3 : 'Pneumonia'}
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods = ['GET'])
-def home_page():
-    data_set = {'Page': 'Home', 'Message': "Let's get started and send me your image"}
-    json_dump = json.dumps(data_set)
-    return json_dump
-  
-if __name__=='__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+def predict_label(img_path):
+	i = image.load_img(img_path, target_size=(64,64))
+	i = image.img_to_array(i)/255.0
+	i = i.reshape(1, 64,64,3)
+	p = model.predict_classes(i)
+	return dic[p[0]]
+
+@app.route("/", methods=['GET', 'POST'])
+def main():
+	return render_template("index.html")
+
+@app.route("/about")
+def about_page():
+	return "This Project is Designed by The Students of The Faculty of Electronic Engineering - Egypt"
+
+@app.route("/submit", methods = ['GET', 'POST'])
+def get_output():
+	if request.method == 'POST':
+		img = request.files['my_image']
+
+		img_path = "static/" + img.filename	
+		img.save(img_path)
+
+		p = predict_label(img_path)
+
+	return render_template("index.html", prediction = p, img_path = img_path)
+
+
+if __name__ =='__main__':
+	#app.debug = True
+	app.run(debug = True)
