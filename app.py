a6751d990143e260488ed8660405020b63a7be20
ME3ZA <101148207+ME3ZA@users.noreply.github.com>
2022-05-26 21:32:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,31 @@
-from flask import *
-import json
-import os
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-from flask_cors import CORS
+from flask import Flask, render_template, request
 from keras.models import load_model
 from keras.preprocessing import image
 
-
-filename = 'chest_model_balanced.h5'
-model = keras.models.load_model(filename)
-
-dic = {0 : 'Covid', 1 : 'Healthy' , 2 : 'Lung Tumor' , 3 : 'Pneumonia'}
-
 app = Flask(__name__)
-CORS(app)
+
+dic = {0 : 'Cat', 1 : 'Dog'}
+
+model = load_model(r'C:\Users\user\Desktop\Graduation\chest_model_balanced.h5')
+
+model.make_predict_function()
 
 def predict_label(img_path):
-	i = image.load_img(img_path, target_size=(64,64))
+	i = image.load_img(img_path, target_size=(100,100))
 	i = image.img_to_array(i)/255.0
-	i = i.reshape(1, 64,64,3)
+	i = i.reshape(1, 100,100,3)
 	p = model.predict_classes(i)
 	return dic[p[0]]
 
+
+# routes
 @app.route("/", methods=['GET', 'POST'])
 def main():
 	return render_template("index.html")
 
 @app.route("/about")
 def about_page():
-	return "This Project is Designed by The Students of The Faculty of Electronic Engineering - Egypt"
+	return "Please subscribe  Artificial Intelligence Hub..!!!"
 
 @app.route("/submit", methods = ['GET', 'POST'])
 def get_output():
@@ -45,6 +40,6 @@ def get_output():
 	return render_template("index.html", prediction = p, img_path = img_path)
 
 
-if __name__ =='__main__':
-	#app.debug = True
-	app.run(debug = True)
+if __name__=='__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
