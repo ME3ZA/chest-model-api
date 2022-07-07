b9ff54b5f6686f7ab1979727bd98ec5d0e9b6df8
ME3ZA <101148207+ME3ZA@users.noreply.github.com>
2022-07-07 20:05:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,26 @@
 from flask import *
-from keras.models import load_model
-from keras.preprocessing import image
 import json
 import os
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+
 from flask_cors import CORS
+
+
+filename = 'chest_model_balanced.h5'
+model = keras.models.load_model(filename)
+
 
 app = Flask(__name__)
 CORS(app)
 
-filename1 = 'chest_model_balanced.h5'
-model = keras.models.load_model(filename1)
-
-# routes
-@app.route("/", methods=['GET', 'POST'])
-def main():
-	return render_template("index.html")
-
-@app.route("/about")
-def about_page():
-	return "This Project Was Designed By The Students of Faculty of Electronic Engineering - Egypt"
-
-
+@app.route('/', methods = ['GET'])
+def home_page():
+    data_set = {'Page': 'Home', 'Message': "Let's get started and send me your image"}
+    json_dump = json.dumps(data_set)
+    return json_dump
+  
 if __name__=='__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
